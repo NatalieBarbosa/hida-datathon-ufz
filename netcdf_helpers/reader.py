6e8e84b956ea12d15c0c090120f788e8fe3e5419
Skhurana <62461235+Skhurana136@users.noreply.github.com>
2020-04-02 11:50:07 +0200

    Update reader.py

--- netcdf_helpers/reader.py
+++ netcdf_helpers/reader.py
@@ -41,9 +41,27 @@ def get_time_series_from_location(data, var_name, lat_target, lon_target):
         id = np.argmin(diff_array)
         return id
 
+    def getclosest_ij(latarray,longarray,lat_target,long_target):
+        # find squared distance of every point on grid
+        leastdist = 1000000
+        latindx = np.arange(0,len(latarray),1)
+        lonindx = np.arange(0,len(longarray),1)
+        for y,x in zip(latindx, lonindx):
+            dist_sq = (latarray[y]-lat_target)**2 + (longarray[x]-long_target)**2
+#            print(dist_sq)
+            dist_sq_old = dist_sq
+            if dist_sq < leastdist or dist_sq < dist_sq_old:
+                leastdist = dist_sq
+                leastlatidx = y
+                leastlongidx = x
+                leastdist = dist_sq
+#                print(leastdist, latarray[y], longarray[x])  
+        print ("The closes coordinates are: "+ str(leastdist)+ " meters away")
+        return leastlatidx, leastlongidx
     # get the index for target point
-    lat_indx_target = near(lat, lat_target)
-    lon_indx_target = near(lon, lon_target)
+#    lat_indx_target = near(lat, lat_target)
+#    lon_indx_target = near(lon, lon_target)
+    lat_indx_target, lon_indx_target = getclosest_ij(lat, lon, lat_target, lon_target)
 
     # get the value of that index
     lat_val_target = lat[lat_indx_target]
