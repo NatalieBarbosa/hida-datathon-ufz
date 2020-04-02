a0bf23a81af995c797a4b6de54e0ac1cae322d85
Skhurana <62461235+Skhurana136@users.noreply.github.com>
2020-04-02 19:39:20 +0200

    Updated clustering baseline file
    Will be great if we could see global temperature maps of the filtered time series (values to look for:
    [5300716.0,
     5310716.0,
     5320716.0,
     5330716.0,
     5340716.0,
     5350716.0,
     5670716.0,
     6750716.0,
     6770716.0,
     6780716.0,
     6790716.0,
     7430716.0,
     8130716.0,
     8160716.0])

--- SK_Clustering_WIP.py
+++ SK_Clustering_WIP.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
 import os.path as osp
-import netCDF4
-from netcdf_helpers.reader import say_hello, get_time_series_from_location
-from plot.plot import plot_time_series_for_locations
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+#import netCDF4
+#from netcdf_helpers.reader import say_hello, get_time_series_from_location
+#from plot.plot import plot_time_series_for_locations
+#from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import xarray as xr
-import pandas as pd
-import cartopy.crs as ccrs
+#import pandas as pd
+#import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import mglearn
 
 say_hello()
 
@@ -26,39 +26,52 @@ filename_volc_data = "Volc_Forc_AOD_1st_mill.nc"
 temp_data_r1 = xr.open_dataset(osp.join(directory, filename_temp_data_r1))
 temp_data_r2 = xr.open_dataset(osp.join(directory, filename_temp_data_r2))
 
-df_r1 = temp_data_r1.to_dataframe()["T2m"]
-df_r1.index.names
-print(df_r1.index.get_level_values('time'))
-timelist = df_r1.index.get_level_values('time')
+#Understand the data more and see the levels of each column
+df = temp_data_r1.to_dataframe()["T2m"]
+print(df.index.get_level_values('time'))
+timelist = df.index.get_level_values('time')
+latlist = df.index.get_level_values('lat')
+lonlist = df.index.get_level_values('lon')
 
+#Reset the indices (I find it easier to work this way)
+df_r1 = temp_data_r1.to_dataframe().reset_index(level=['lat', 'lon', 'time'])#["T2m"]
+#Calculate a global annual mean temperature time series
 Globalmeantemp = df_r1.groupby('time').mean()
-mean = np.mean(Globalmeantemp)
-Var_frommean = Globalmeantemp - mean
-plt.plot(Globalmeantemp)
+#Calculate the mean of the time series to focus on the variation from the mean
+mean = np.mean(Globalmeantemp["T2m"])
+Var_frommean = Globalmeantemp["T2m"] - mean
 plt.plot(Var_frommean)
 
 from sklearn.cluster import KMeans
+#Initialize the algorithm and fit it with the data
 kmeans = KMeans(n_clusters = 5)
 X = Var_frommean.to_numpy().reshape(-1,1)
 kmeans.fit(X)
 print("Cluster memberships:\n{}".format(kmeans.labels_))
+#Assign classes to each data point based on the model
 classes = kmeans.predict(X)
-dip = np.argwhere(classes==4)
+#Inspect the centroids of the clusters
+print(kmeans.cluster_centers_)
+#Shortcut to see/visualize the datapoints and range of each cluster
+mglearn.discrete_scatter(X, X, kmeans.labels_, markers='o')
+#Volcanic activity is expected to have the maximum impact out of all forcings so look for the time points which are in the cluster associated with the lowest centroid
+dip = np.argwhere(classes==np.argmin(kmeans.cluster_centers_))
+#look for the years which have the biggest dips
 dipinyear = list(int(timelist[i][0]/10000) for i in dip)
 len(dipinyear)
-print(kmeans.predict(X))
 
-import mglearn
-mglearn.discrete_scatter(X, X, kmeans.labels_, markers='o')
-mglearn.discrete_scatter(kmeans.cluster_centers_, kmeans.cluster_centers_, [0,1,2], markers='^', markeredgewidth=2)
+shortlistedtimeseries = list(timelist[i][0] for i in dip)
 
-X = r1_df.loc[r1_df.time < 30]
-kmeans.fit(X["T2m"].to_numpy().reshape(-1,1))
-mglearn.discrete_scatter(X["lon"], X["lat"], kmeans.labels_, markers='o')
-mglearn.discrete_scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], [0,1,2], markers='^', markeredgewidth=2)
-
-print("Cluster memberships:\n{}".format(kmeans.labels_))
-classes = kmeans.predict(X)
-dip = np.argwhere(classes==2)
-dipinyear = list(timelist[i] for i in dip)
-print(kmeans.predict(X))
+#Filter the dataset and look at only the years which have the biggest dips for the data analysis/image analysis
+#Also divide it into 6 zones as previously discussed: tropical, temperate and polar in northern and southern hemispheres
+df_r1_time = df_r1[df_r1.time.isin(shortlistedtimeseries)]
+df_North = df_r1[df_r1.lat>0]
+df_North_trop = df_North[df_North.lat<30]
+df_North_nottrop = df_North[df_North.lat>30]
+df_North_temp = df_North_nottrop[df_North_nottrop.lat<60]
+df_North_polar = df_North_nottrop[df_North_nottrop.lat>60]
+df_South = df_r1[df_r1.lat<0]
+df_South_trop = df_South[df_South.lat>-30]
+df_South_nottrop = df_South[df_South.lat<-30]
+df_South_temp = df_South_nottrop[df_South_nottrop.lat>-60]
+df_South_polar = df_South_nottrop[df_South_nottrop.lat<-60]
